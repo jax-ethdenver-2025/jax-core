35f0ed836dfff07a740ca6e425b49dcae0db6290
xBalbinus <xiangan9@gmail.com>
2025-02-28 13:56:04 -0700

    avs integration

--- contracts/test/mocks/AVSMock.sol
+++ contracts/test/mocks/AVSMock.sol
@@ -1,9 +1,26 @@
 pragma solidity ^0.8.20;
 
-import {IAVS} from "../../src/interface/IAVS.sol";
+import "../src/interface/IIncredibleSquaringTaskManager.sol";
 
-contract AVSMock is IAVS {
-    function getWalletProviders() public pure override returns (address[] memory) {
-        return new address[](0);
+contract AVSMock is IIncredibleSquaringTaskManager {
+    function createNewTask(bytes32 fileHash, uint32 quorumThresholdPercentage, bytes calldata quorumNumbers) external override {
+        // Mock implementation
+    }
+
+    function taskNumber() external view override returns (uint32) {
+        return 0; // Mock implementation
+    }
+
+    function raiseAndResolveChallenge(
+        Task calldata task,
+        TaskResponse calldata taskResponse,
+        TaskResponseMetadata calldata taskResponseMetadata,
+        BN254.G1Point[] memory pubkeysOfNonSigningOperators
+    ) external override {
+        // Mock implementation
+    }
+
+    function getTaskResponseWindowBlock() external view override returns (uint32) {
+        return 0; // Mock implementation
     }
 }
